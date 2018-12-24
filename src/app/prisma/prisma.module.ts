05b8c1db7001eb8f24436055ce653ca76dd699cc
Chanlito <chanlitohem@gmail.com>
2018-12-24 19:42:19 +0700

    :recycle: tweak prisma module

--- src/app/prisma/prisma.module.ts
+++ src/app/prisma/prisma.module.ts
@@ -2,11 +2,13 @@ import { GraphQLModule } from '@graphql-modules/core';
 import { ProviderScope } from '@graphql-modules/di';
 
 import {
+  Prisma as PrismaBinding,
+  prismaBinding,
+} from '@/lib/prisma/prisma-binding';
+import {
   Prisma as PrismaClient,
   prisma as prismaClient,
 } from '@/lib/prisma/prisma-client';
-
-import { Prisma as PrismaBinding, prismaBinding } from '@/lib/prisma/prisma-binding';
 
 export const PrismaModule = new GraphQLModule({
   providers: [
